359fce1728d9dd20f8c133e6eb74aee64aa0780e
Callum Rogers <callumrogers@hotmail.co.uk>
2016-02-13 22:29:24 +0000

    Include some typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 module.exports = {
-  entry: 'file?name=index.html!jade-html!./src/jade/index.jade',
+  entry: [
+    'file?name=index.html!jade-html!./src/jade/index.jade',
+    './src/ts/hello.ts'
+  ],
   output: {
     path: __dirname + '/build',
     publicPath: '/build/',
-    filename: 'ignore-this'
+    filename: 'app.js'
   },
   module: {
     loaders: [
-      {test: /\.jade/, loader: 'jade-html-loader'}
+      {test: /\.jade/, loader: 'jade-html-loader'},
+      {test: /\.ts/, loader: 'ts-loader'}
     ]
   }
 };
