54dbe6fe2eeb206ad49c3aa301ce0fbd6a0d4c2d
Callum Rogers <callumrogers@hotmail.co.uk>
2016-02-13 21:36:46 +0000

    Setup git users

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git config user.email "circleci-build-node@circleci.com"
+git config user.name "CircleCI build node"
+
 HASH_MESSAGE="$(git show --oneline | head -n1)"
 
 git checkout --orphan gh-pages
